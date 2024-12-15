9fba006f1b4deaf3c3f18a7554277405d8be1d7a
th0rOdinson <0xth0rOdinson@gmail.com>
2024-12-15 00:16:08 -0300

    adding zk

--- packages/nextjs/constants/index.tsx
+++ packages/nextjs/constants/index.tsx
@@ -1,6 +1,6 @@
 import { Address } from "viem";
 import addresses from "~~/contracts-data/deployments/addresses.json";
-import { OptimismSepoliaChainId } from "~~/contracts/addresses";
+import { OptimismSepoliaChainId, zkSyncTestNetCode } from "~~/contracts/addresses";
 
 export const BASE_URL =
   process.env.NODE_ENV === "production"
@@ -13,4 +13,5 @@ type deployedContracts = keyof (typeof addresses)["optimismSepolia"];
 
 export const ContractAddresses: Record<number, Record<deployedContracts, Address>> = {
   [OptimismSepoliaChainId]: addresses["optimismSepolia"] as Record<deployedContracts, Address>,
+  [zkSyncTestNetCode]: addresses["zkSyncSepolia"] as Record<deployedContracts, Address>,
 };
